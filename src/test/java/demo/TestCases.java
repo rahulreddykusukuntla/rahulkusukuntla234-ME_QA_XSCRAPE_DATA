be4866b5cc9b015428b446a5716537269fd3ba52
rahulkusukuntla234 <rahulkusukuntla234@gmail.com>
2024-08-30 16:21:55 +0530

    ME_QA_XSCRAPE_DATA_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,18 +1,31 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Level;
+import java.io.File;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
+import java.time.Duration;
 
 public class TestCases {
     ChromeDriver driver;
@@ -21,7 +34,108 @@ public class TestCases {
      * TODO: Write your tests here with testng @Test annotation. 
      * Follow `testCase01` `testCase02`... format or what is provided in instructions
      */
+    @Test
+    public void testCase01() throws InterruptedException, IOException{
+        ObjectMapper mapper = new ObjectMapper();
+        driver.get("https://www.scrapethissite.com/pages/");
+        HashMap<Integer,Object> map=new HashMap<Integer,Object>();
+        Wrappers.clickHockeyTeams(driver);
+        int k=1,n=0;
+        // Wrappers.getTableDetails(driver);
+        // ArrayList<ArrayList<String>> li=new ArrayList<ArrayList<String>>();
+        while(n<4){
+            int rows=driver.findElements(By.xpath("//table/tbody/tr")).size();
+            for(int i=2;i<rows;i++){
+                String val=driver.findElement(By.xpath("//table/tbody/tr["+i+"]/td[6]")).getText();
+                float f=Float.parseFloat(val);
+                if(f<0.4){
+                    ArrayList<String> list=new ArrayList<>();
+                    String team=driver.findElement(By.xpath("//table/tbody/tr["+i+"]/td[1]")).getText();
+                    String year=driver.findElement(By.xpath("//table/tbody/tr["+i+"]/td[2]")).getText();
+                // System.out.println(team);
+                // System.out.println(year);
+                // System.out.println(val);
+                // System.out.println(Long.toString(getEpochTime()));
+                    list.add(Long.toString(Wrappers.getEpochTime()));
+                    list.add(team);
+                    list.add(year);
+                    list.add(val);
+                // li.add(list);
+                    map.put(k,list);
+                    k++;
 
+                
+                }
+
+            }
+            Wrappers.clickNext(driver);
+            n++;
+        }
+        for(Integer i:map.keySet()){
+            System.out.println(map.get(i));
+        }
+        Wrappers.createJsonFile(mapper,map);
+        String userDir = System.getProperty("user.dir");
+        try {
+            mapper.writerWithDefaultPrettyPrinter()
+                    .writeValue(new File(userDir + "\\src\\test\\resources\\hockey-team-data.json"), map);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+    }
+    @Test
+    public void testCase02() throws IOException{
+        ObjectMapper mapper = new ObjectMapper();
+        int q=1;
+        WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(5));
+        driver.get("https://www.scrapethissite.com/pages/");
+        HashMap<Integer,Object> map=new HashMap<Integer,Object>();
+        Wrappers.clickOscarWinning(driver);
+        for(WebElement i:Wrappers.getYears(driver)){
+            String year=i.getText();
+            i.click();
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//th[text()='Title']")));
+            for(int j=1;j<=5;j++){
+                ArrayList<String> list=new ArrayList<>();
+                list.add(Long.toString(Wrappers.getEpochTime()));
+                list.add(year);
+                for(int k=1;k<=4;k++){
+                    
+                    if(k<4){
+                        String str=driver.findElement(By.xpath("//table/tbody/tr["+j+"]/td["+k+"]")).getText();
+                        list.add(str);
+
+                    }
+                    else{
+                        try{
+                            Boolean isWinner=driver.findElement(By.xpath("//table/tbody/tr["+j+"]/td["+k+"]/i")).isDisplayed();
+                            list.add(String.valueOf(isWinner));
+                        }catch (Exception e) {
+                            list.add("false");
+                        }
+
+                        
+                    }
+
+                }
+                map.put(q,list);
+                q++;
+            }
+
+        }
+        for(Integer i:map.keySet()){
+            System.out.println(map.get(i));
+        }
+        Wrappers.createJsonFile(mapper,map);
+        String userDir = System.getProperty("user.dir");
+        try {
+            mapper.writerWithDefaultPrettyPrinter()
+                    .writeValue(new File(userDir + "\\src\\test\\resources\\oscar-winner-data.json"), map);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
      
     /*
      * Do not change the provided methods unless necessary, they will help in automation and assessment
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -3,17 +3,74 @@ package demo.wrappers;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import java.io.File;
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashMap;
 import java.time.Duration;
 
 public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+    // static WebDriver driver=new ChromeDriver();
+
+    public static long getEpochTime(){
+        long epochSeconds = Instant.now().getEpochSecond();
+        // System.out.println("Epoch in Seconds: " + epochSeconds);
+        return epochSeconds;
+    }
+
+    public static void clickNext(WebDriver driver) throws InterruptedException{
+        WebElement ele=driver.findElement(By.xpath("//a[@aria-label='Next']"));
+        ele.click();
+        Thread.sleep(3000);
+    }
+
+    public static void clickHockeyTeams(WebDriver driver){
+        WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement link=driver.findElement(By.xpath("//a[text()='Hockey Teams: Forms, Searching and Pagination']"));
+        link.click();
+        wait.until(ExpectedConditions.urlContains("/pages/forms/"));
+
+    }
+    
+    public static void clickOscarWinning(WebDriver driver){
+        WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(5));
+        WebElement link=driver.findElement(By.xpath("//a[text()='Oscar Winning Films: AJAX and Javascript']"));
+        link.click();
+        wait.until(ExpectedConditions.urlContains("ajax-javascript"));
+
+    }
+
+    public static List<WebElement> getYears(WebDriver driver){
+        List<WebElement> list=driver.findElements(By.xpath("//a[@class='year-link']"));
+        return list;
+
+    }
+
+    public static void createJsonFile(ObjectMapper mapper,HashMap<Integer,Object> map) throws IOException{
+         try {
+            String Json = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(map);
+            System.out.println(Json);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        
+        
+    }
+
 }
